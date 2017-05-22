9f3c9e60d2be04af045a75ff829d82db577f772c
gfed53 <gfederico7153@gmail.com>
2017-05-22 16:32:51 -0400

    Now using jcarousel instead of jssor for header carousel

--- js/script.js
+++ js/script.js
@@ -3,7 +3,17 @@
 $(function(){
 
 	console.log('hello');
-	
+	$('.jcarousel').jcarousel({
+        // Configuration goes here
+        list: '.jcarousel-list',
+        items: '.jcarousel-item',
+        transitions: true,
+        wrap: 'circular',
+        center: true
+    });
+
+    $('.jcarousel').jcarouselAutoscroll();
+    
 	function init(){
 	}
 	//
