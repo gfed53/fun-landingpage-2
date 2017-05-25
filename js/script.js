8e24b384a89dccf37f43e9d993ff2ed1a11a12dd
gfed53 <gfederico7153@gmail.com>
2017-05-25 16:40:47 -0400

    Restructuring, adding of scroll animation to info components

--- js/script.js
+++ js/script.js
@@ -3,6 +3,8 @@
 $(function(){
 
 	console.log('hello');
+
+	//Carousel Init
 	$('.jcarousel').jcarousel({
         // Configuration goes here
         list: '.jcarousel-list',
@@ -18,5 +20,36 @@ $(function(){
 	function init(){
 	}
 	//
+
+	//Scroll Animations
+	var $window = $(window);
+	var $infoElems = $('.info-inner');
+
+	$window.on('scroll resize', checkIfInView);
+
+	$window.trigger('scroll');
+
+	function checkIfInView(){
+		var window_height = $window.height();
+		var window_top_position = $window.scrollTop();
+		var window_bottom_position = (window_top_position + window_height);
+
+		$.each($infoElems, function() {
+			var $element = $(this);
+			var element_height = $element.outerHeight();
+			var element_top_position = $element.offset().top;
+			var element_bottom_position = (element_top_position + element_height);
+
+			//check to see if this current container is within viewport
+			if ((element_bottom_position >= window_top_position) &&
+			(element_top_position <= window_bottom_position)) {
+				console.log('class added to', $element);
+			$element.addClass('in-view');
+			} else {
+				console.log('class removed from', $element);
+				$element.removeClass('in-view');
+			}
+		});
+	}
 
 });
